11c82efcdedd053e790da25d36ffd42d02814ce7
YuryHu <yhu06351@gmail.com>
2023-09-17 10:49:18 +0300

    feat: task is solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.candidate = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.candidate = Math.round((this.min + this.max)/2);
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
